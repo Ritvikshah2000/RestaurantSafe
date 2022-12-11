33eabea5d3bb016f0d9b4f7017658113dcef67dc
ritviks <ritvik_shah@sfu.ca>
2022-12-10 18:35:18 -0800

    setup news carousel

--- app/src/main/java/com/example/restaurantsafe/LoggedInMenu.java
+++ app/src/main/java/com/example/restaurantsafe/LoggedInMenu.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoggedInMenu extends AppCompatActivity {
     private static boolean isPremiumUser = false;
+
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,14 @@ public class LoggedInMenu extends AppCompatActivity {
         setContentView(R.layout.logged_in_menu);
         Button helpBtn = (Button) findViewById(R.id.profile_btn);
         Button signOutBtn = (Button) findViewById(R.id.sign_out_btn);
+
+        int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for (int image: images){
+            flipperImages(image);
+        }
 
 //        btnPress(helpBtn, Profile.makeIntent(LoggedInMenu.this));
 //        btnPress(signOutBtn, SignOut.makeIntent(LoggedInMenu.this));
@@ -37,5 +49,19 @@ public class LoggedInMenu extends AppCompatActivity {
 
     public static void setIsPremiumUser(boolean isPremiumUser) {
         LoggedInMenu.isPremiumUser = isPremiumUser;
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000); //4 sec
+        v_flipper.setAutoStart(true);
+
+        //animation
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
     }
 }
